5f1028429c66217a0b30f2e83a6de0c252bd6df0
Andreas Klos <andreas.klos@fernuni-hagen.de>
2021-08-09 18:04:48 +0200

    ADD test for conv2d and adjusted tests for its base class

--- test_baseconv.py
+++ test_baseconv.py
@@ -1,7 +1,9 @@
 import unittest
 
-from baseconv import LayerBase, Conv2DBase
+from baseconv import Conv2D, LayerBase, Conv2DBase
 import torch
+
+from math import ceil
 
 class TestLayerBase(unittest.TestCase):
 
@@ -39,7 +41,8 @@ class TestConv2DBase(unittest.TestCase):
         b, c, x, y = [2,20,9,9]
         rnd_input = torch.rand(size=[b,c,x,y])
 
-        bn_out = self.conv2dbase.bn(rnd_input)
+        bn = self.conv2dbase._apply_normalization(out_channels)
+        bn_out = bn(rnd_input)
         self.assertAlmostEqual(bn_out.mean().item(),0.0,places=1)
         self.assertAlmostEqual(bn_out.std().item(),1.0,places=1)
 
@@ -49,18 +52,86 @@ class TestConv2DBase(unittest.TestCase):
         b, c, x, y = [1,3,9,9]
         rnd_input = torch.rand(size=[b,c,x,y])
         output = self.conv2dbase(rnd_input)
-        self.assertEqual(list(output.size()),[1,10,7,7])
-        
-        kernel_size = [3,3]
-        padding = [(kernel_size[0]-1)//2,(kernel_size[0]-1)//2,(kernel_size[1]-1)//2,(kernel_size[1]-1)//2]
-        self.conv2dbase = Conv2DBase(in_channels,out_channels,kernel_size=kernel_size,padding=padding)
-
-        output = self.conv2dbase(rnd_input)
-        self.assertEqual(list(output.size()),[b,out_channels,x,y])
-
+        self.assertEqual(list(output.size()),[1,3,9,9])
         
 
+class TestConv2D(unittest.TestCase):
+    def setUp(self) -> None:
+        self.in_channels = 3
+        self.out_channels=10
+        self.kernel_size = [3,3]
+        self.padding = [
+            self.kernel_size[0]//2,
+            self.kernel_size[0]//2,
+            self.kernel_size[1]//2,
+            self.kernel_size[1]//2
+        ]
+        self.stride = [1,1]
+        self.batch_size = 16
+        self.input_width = self.input_height=32
+        self.conv2d = Conv2D(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            kernel_size=self.kernel_size, 
+            stride=self.stride,
+            in_size=[self.input_width,self.input_height]            
+        )
 
+    def test_forward(self):
+        x = torch.rand([self.batch_size,self.in_channels,self.input_width,self.input_height])
+        self.assertTrue(
+            self.conv2d(x).size()
+                ==
+            torch.Size(
+                (
+                    self.batch_size,self.out_channels,self.input_width,self.input_height
+                )
+            )
+        )
+        
+        self.stride=[3,3]
+        self.conv2d = Conv2D(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            kernel_size=self.kernel_size, 
+            stride=self.stride,
+            in_size=[self.input_width,self.input_height]            
+        )
+        
+        self.assertTrue(
+            self.conv2d(x).size()
+            ==
+            torch.Size(
+                (
+                    self.batch_size,
+                    self.out_channels,
+                    ceil(self.input_width/self.stride[0]),
+                    ceil(self.input_height/self.stride[1])
+                )
+            )
+        )
+
+        self.kernel_size=[7,7]
+        self.conv2d = Conv2D(
+            in_channels=self.in_channels,
+            out_channels=self.out_channels,
+            kernel_size=self.kernel_size, 
+            stride=self.stride,
+            in_size=[self.input_width,self.input_height]            
+        )
+        
+        self.assertTrue(
+            self.conv2d(x).size()
+            ==
+            torch.Size(
+                (
+                    self.batch_size,
+                    self.out_channels,
+                    ceil(self.input_width/self.stride[0]),
+                    ceil(self.input_height/self.stride[1])
+                )
+            )
+        )
 
 
 
