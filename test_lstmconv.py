39cc1bfbca3d50605f4e489e0bfbe48a7b614274
Andreas Klos <andreas.klos@fernuni-hagen.de>
2021-08-10 23:56:41 +0200

    UNCOMMNETED stuff from unittest file

--- test_lstmconv.py
+++ test_lstmconv.py
@@ -100,55 +100,55 @@ class TestConvLSTM(unittest.TestCase):
   
 
 
-    # def test_initialize(self):
-    #     self.conv_lstm = ConvLSTM(
-    #         in_channels=self.in_channels,
-    #         hidden_channels=self.hidden_channels,
-    #         kernel_size=self.kernel_size,
-    #         stride=self.stride,
-    #         in_size=[self.input_width,self.input_height],
-    #         time_steps=self.time_steps
-    #     )
+    def test_initialize(self):
+        self.conv_lstm = ConvLSTM(
+            in_channels=self.in_channels,
+            hidden_channels=self.hidden_channels,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            in_size=[self.input_width,self.input_height],
+            time_steps=self.time_steps
+        )
 
-    #     self.assertEqual(self.conv_lstm.Wci,None)
-    #     self.assertEqual(self.conv_lstm.Wcf,None)
-    #     self.assertEqual(self.conv_lstm.Wco,None)
+        self.assertEqual(self.conv_lstm.Wci,None)
+        self.assertEqual(self.conv_lstm.Wcf,None)
+        self.assertEqual(self.conv_lstm.Wco,None)
         
-    #     self.conv_lstm.initialize_gates(self.input_width,self.input_height)
+        self.conv_lstm.initialize_gates(self.input_width,self.input_height)
         
-    #     self.assertNotEqual(self.conv_lstm.Wci,None)
-    #     self.assertNotEqual(self.conv_lstm.Wcf,None)
-    #     self.assertNotEqual(self.conv_lstm.Wco,None)
+        self.assertNotEqual(self.conv_lstm.Wci,None)
+        self.assertNotEqual(self.conv_lstm.Wcf,None)
+        self.assertNotEqual(self.conv_lstm.Wco,None)
 
-    #     self.assertTrue(self.conv_lstm.Wci.requires_grad)
-    #     self.assertTrue(self.conv_lstm.Wcf.requires_grad)
-    #     self.assertTrue(self.conv_lstm.Wco.requires_grad)
+        self.assertTrue(self.conv_lstm.Wci.requires_grad)
+        self.assertTrue(self.conv_lstm.Wcf.requires_grad)
+        self.assertTrue(self.conv_lstm.Wco.requires_grad)
 
-    #     self.assertTrue(
-    #         self.conv_lstm.Wci.size()
-    #             ==
-    #         torch.Size(
-    #             (
-    #                 1,self.hidden_channels,self.input_width,self.input_height
-    #             )
-    #         )
-    #     ) 
-    #     self.assertTrue(
-    #         self.conv_lstm.Wcf.size()
-    #             ==
-    #         torch.Size(
-    #             (
-    #                 1,self.hidden_channels,self.input_width,self.input_height
-    #             )
-    #         )
-    #     ) 
-    #     self.assertTrue(
-    #         self.conv_lstm.Wco.size()
-    #             ==
-    #         torch.Size(
-    #             (
-    #                 1,self.hidden_channels,self.input_width,self.input_height
-    #             )
-    #         )
-    #     ) 
+        self.assertTrue(
+            self.conv_lstm.Wci.size()
+                ==
+            torch.Size(
+                (
+                    1,self.hidden_channels,self.input_width,self.input_height
+                )
+            )
+        ) 
+        self.assertTrue(
+            self.conv_lstm.Wcf.size()
+                ==
+            torch.Size(
+                (
+                    1,self.hidden_channels,self.input_width,self.input_height
+                )
+            )
+        ) 
+        self.assertTrue(
+            self.conv_lstm.Wco.size()
+                ==
+            torch.Size(
+                (
+                    1,self.hidden_channels,self.input_width,self.input_height
+                )
+            )
+        ) 
     
