3f00a0583ac7d216207996f2f0ac92a5dcd97164
Andreas Klos <andreas.klos@fernuni-hagen.de>
2021-08-13 01:01:49 +0200

    MAKE GRU implementation more efficient (about 10 %)

--- convgru.py
+++ convgru.py
@@ -49,7 +49,7 @@ class ConvGRUFullyGated(Conv2DBase):
         self.time_steps = time_steps
 
         self.padding_t = [kernel_size[0]//2,kernel_size[0]//2,kernel_size[1]//2,kernel_size[1]//2]
-        self.add_module("same_padding_t",nn.ReplicationPad2d(self.padding_t) if self.padding_t!=0 or (len(self.padding_t)>1 and sum(self.padding_t)>=1) else None) 
+        self.add_module("same_padding_t",nn.ReplicationPad2d(self.padding_t)) 
         self.add_module(
             "conv_w",nn.Conv2d(
                 in_channels=self.in_channels,
@@ -57,37 +57,26 @@ class ConvGRUFullyGated(Conv2DBase):
                 kernel_size=self.kernel_size,
                 stride=[1,1],
                 padding=[0,0],
-                bias=False if config.employ_batch_normalization_conv else True
+                bias=False
             )
         )
         self.add_module(
-            "bn_w", nn.ModuleList(
-                [
-                    nn.BatchNorm2d(
-                        num_features=3* hidden_channels,
-                        momentum=config.batch_normalization_momentum
-                    ) 
-                    for i in range(time_steps)
-                ]) if config.employ_batch_normalization_conv else None
+            "bn_w", 
+            nn.ModuleList([
+                nn.BatchNorm2d(
+                    num_features=3*hidden_channels,
+                    momentum=config.batch_normalization_momentum
+                ) for i in range(self.time_steps)
+            ])
         )
         self.add_module(
-            "conv_uz",nn.Conv2d(
+            "conv_uzr",nn.Conv2d(
                 in_channels=self.hidden_channels,
-                out_channels=self.hidden_channels,
+                out_channels=2*self.hidden_channels,
                 kernel_size=self.kernel_size,
                 stride=[1,1],
                 padding=[0,0],
-                bias=False if config.employ_batch_normalization_conv else True
-            )
-        )
-        self.add_module(
-            "conv_ur",nn.Conv2d(
-                in_channels=self.hidden_channels,
-                out_channels=self.hidden_channels,
-                kernel_size=self.kernel_size,
-                stride=[1,1],
-                padding=[0,0],
-                bias=False if config.employ_batch_normalization_conv else True
+                bias=False 
             )
         )
         self.add_module(
@@ -97,38 +86,27 @@ class ConvGRUFullyGated(Conv2DBase):
                 kernel_size=self.kernel_size,
                 stride=[1,1],
                 padding=[0,0],
-                bias=False if config.employ_batch_normalization_conv else True
+                bias=False 
             )
         )
         self.add_module(
-            "bn_uz", nn.ModuleList(
-                [
-                    nn.BatchNorm2d(
-                        num_features=hidden_channels,
-                        momentum=config.batch_normalization_momentum
-                    ) 
-                    for i in range(time_steps-1)
-                ]) if config.employ_batch_normalization_conv else None
+            "bn_uzr",
+            nn.ModuleList([
+                nn.BatchNorm2d(
+                    num_features=2*hidden_channels,
+                    momentum=config.batch_normalization_momentum
+                ) for i in range(self.time_steps-1)
+            ])    
         )
+
         self.add_module(
-            "bn_ur", nn.ModuleList(
-                [
-                    nn.BatchNorm2d(
-                        num_features=hidden_channels,
-                        momentum=config.batch_normalization_momentum
-                    ) 
-                    for i in range(time_steps-1)
-                ]) if config.employ_batch_normalization_conv else None
-        )
-        self.add_module(
-            "bn_u", nn.ModuleList(
-                [
-                    nn.BatchNorm2d(
-                        num_features=hidden_channels,
-                        momentum=config.batch_normalization_momentum
-                    ) 
-                    for i in range(time_steps-1)
-                ]) if config.employ_batch_normalization_conv else None
+            "bn_u",
+            nn.ModuleList([
+                nn.BatchNorm2d(
+                    num_features=hidden_channels,
+                    momentum=config.batch_normalization_momentum
+                ) for i in range(self.time_steps-1)
+            ]) 
         )
 
 
@@ -149,28 +127,24 @@ class ConvGRUFullyGated(Conv2DBase):
         
         h_cur = torch.zeros((batch_size,self.hidden_channels,width,height),dtype=torch.float).to(self.device)
                 
-        if self.same_padding_t!=None:
-            input_tensor=self.same_padding_t(input_tensor)
+        
+        input_tensor=self.same_padding_t(input_tensor)
         input_tensor_conv = self.conv_w(input_tensor)
-               
-        for t in range(self.time_steps):      
-
-            if self.bn_w!=None:
-                input_tensor_conv=self.bn_w[t](input_tensor_conv)
-            z, r, tmp_h = torch.chunk(input_tensor_conv, self.number_of_gates_and_cells, dim=1)
+        
+        for t in range(self.time_steps):
+            input_tensor_conv_bn=self.bn_w[t](input_tensor_conv)
+            z, r, tmp_h = torch.chunk(input_tensor_conv_bn, self.number_of_gates_and_cells, dim=1)
             if t>0:
-                if self.same_padding_t!=None:
-                    h_cur_star = self.same_padding_t(h_cur)
-                z = torch.sigmoid(z+self.bn_uz[t-1](self.conv_uz(h_cur_star))if self.bn_uz[t-1]!=None else self.conv_uz(h_cur_star))
-                r = torch.sigmoid(r+self.bn_ur[t-1](self.conv_ur(h_cur_star))if self.bn_ur[t-1]!=None else self.conv_ur(h_cur_star))
-
-                tmp_h = torch.tanh(tmp_h+self.bn_u[t-1](self.conv_u(self.same_padding_t(r*h_cur)))if self.bn_uz[t-1]!=None else self.conv_u(self.same_padding_t(r*h_cur)))
+                h_cur_star = self.same_padding_t(h_cur)    
+                uz,ur = torch.chunk(self.bn_uzr[t-1](self.conv_uzr(h_cur_star)),2,dim=1)
+                z = torch.sigmoid(z+uz)
+                r = torch.sigmoid(r+ur)
+                tmp_h = torch.tanh(tmp_h+self.bn_u[t-1](self.conv_u(self.same_padding_t(r*h_cur))))
                 h_cur =(1-z)*h_cur+tmp_h*z
             else:
                 z = torch.sigmoid(z)
                 r = torch.sigmoid(r)
                 h_cur = z*torch.tanh(tmp_h)
-
 
         return h_cur
 
